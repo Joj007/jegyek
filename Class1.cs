6b3141fbf6bf0ada3a187cf0aea3e3a75c6b5d62
Joj007 <71085151+Joj007@users.noreply.github.com>
2023-03-09 16:19:26 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp10
+{
+    class Class1
+    {
+        string nev;
+        string targy;
+        string datum;
+        int jegy;
+
+        public Class1(string nev, string targy, string datum, int jegy)
+        {
+            this.Nev = nev;
+            this.Targy = targy;
+            this.Datum = datum;
+            this.Jegy = jegy;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public string Targy { get => targy; set => targy = value; }
+        public string Datum { get => datum; set => datum = value; }
+        public int Jegy { get => jegy; set => jegy = value; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApp10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        StreamWriter sw = new StreamWriter("test.txt");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRogzit_Click(object sender, RoutedEventArgs e)
+        {
+
+            using (StreamWriter sw = new StreamWriter("../../../test.csv", true))
+            {
+                if (txtNev.Text.Length>0&&0>DateTime.Compare(DateTime.Now, DateTime.Parse(dpDatum.Text)))
+                {
+                    MessageBox.Show("Nem megfelelő beviteli értékek");
+                }
+                else
+                {
+                    sw.WriteLine($"{txtNev.Text};{cboTargy.Text};{dpDatum.Text};{lblJegy.Content}");
+                    MessageBox.Show("Ügyi vagy! :(");
+                }
+                
+            }
+        }
+
+        private void sliJegy_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblJegy.Content = sliJegy.Value;
+        }
+    }
+}
